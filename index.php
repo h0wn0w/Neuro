a861d98849382f0315aa20fc892a5c6adb8dc5db
Yacine Benzine <ybenzine@gmail.com>
2011-04-25 03:32:32 +0000

    Initial view.

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+ <title>Project Neuro</title>
+</head>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.js"></script>
+
+<style>
+body {
+  background-color: #000000;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-family: Georgia;
+}
+#back { 
+  background-image: url('background.jpg');
+  position: absolute;
+  top: -600px;
+  left: -700px;
+  width: 3380px;
+  height: 2152px;
+  z-index: 1;
+  -webkit-transition: -webkit-transform 300s linear;
+}
+#content {
+  position: relative;
+  width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 25px;
+  color: #fff;
+  z-index: 3;
+}
+</style>
+<script>
+$(document).ready(function() {
+//  $('#back').style.webkitTransform = 'rotate(360deg)';
+  $('#back').css('-webkit-transform', 'rotate(360deg)');
+  $('#back').animate({left: -300, top: -300}, 120000, function(){});
+});
+</script>
+<body>
+ <div id="back" onclick="//this.style.webkitTransform = 'rotate(360deg)';"></div>
+ <div id="content"> 
+   <h1>Project Neuro</h1>
+   <h4 style="color: #9999FF;; "><?php include 'README'; ?></h4>
+   
+ </div>
+  
+</body>
+</html>
